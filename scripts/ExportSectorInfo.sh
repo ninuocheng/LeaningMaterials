5925a66da1c8ffc1c991919698940bb4b866401e
Gzhichao <zhichaoguo75@gmail.com>
2024-04-19 19:21:05 +0800

    update

--- scripts/ExportSectorInfo.sh
+++ scripts/ExportSectorInfo.sh
@@ -17,6 +17,13 @@ lotus state sectors $MinerID > $AllSector
 #有效的扇区,如果该节点当前既没有错误扇区，也没有要恢复中的扇区和未证明的扇区，那么存活的扇区数量就是当前有效的扇区数量。否则还需要导出错误的扇区，恢复中的扇区和未证明的扇区，才是当前存活的扇区数量
 ActiveSector="$RootDir/ActiveSector"
 lotus state active-sectors $MinerID > $ActiveSector
+#有效扇区的扇区状态
+ActiveStateSector="$RootDir/ActiveStateSector"
+#除去有效扇区之外的扇区
+TerminatedSector="$RootDir/TerminatedSector"
+sort $AllSector $ActiveSector $ActiveSector > $TerminatedSector
+#除去有效扇区之外的扇区状态
+TerminatedStateSector="$RootDir/TerminatedStateSector"
 #所有的扇区信息
 AllSectorInfo="$RootDir/AllSectorInfo"
 #有效的扇区信息
@@ -32,7 +39,7 @@ exec 100<>ch3
 #删除ch3文件防止影响下次的执行（删除后不影响文件描述符的使用）
 rm -f ch3
 #定义用于控制进程数量的变量
-ProcessNub="10"
+ProcessNub="50"
 #通过文件描述符往命名管道中写入任意数据,用于控制进程数量
 for i in `seq $ProcessNub`
 do
@@ -46,21 +53,28 @@ done
 #这时命名管道ch3中数据量是1条，会取消阻塞启动一个新的进程
 #启动之后，ch3中数据量是0条，继续造成阻塞，当有命令执行完成后，就会有新的进程继续执行
 #循环往复，直到执行循环结束
-for i in `awk -F: '{print $1}' $AllSector`
+for i in `awk -F: '{print $1}' $ActiveSector`
 do
 	read -u100
 	{
-		lotus state sector $MinerID $i |awk '/SectorNumber:/{$1="";SectorNumber=$0} /Activation:/{$1="";Activation=$0} /Expiration:/{$1="";Expiration=$0} /InitialPledge:/{$1="";InitialPledge=$0} /ExpectedDayReward:/{$1="";ExpectedDayReward=$0} /ExpectedStoragePledge:/{$1="";ExpectedStoragePledge=$0} /Partition:/ {print SectorNumber,Activation,Expiration,InitialPledge,ExpectedDayReward,ExpectedStoragePledge;SectorNumber="";Activation="";Expiration="";InitialPledge="";ExpectedDayReward="";ExpectedStoragePledge=""}' |sed 's#^[ ]*##g' >> $AllSectorInfo
-		if grep -wq "${i}:" $ActiveSector;then
-			awk '$1 == "'$i'"{print}' $AllSectorInfo >> $ActiveSectorInfo
-		else
-			awk '$1 == "'$i'"{print}' $AllSectorInfo >> $TerminatedSectorInfo
-		fi
+		lotus state sector $MinerID $i >> $ActiveStateSector
 		echo >&100
 	} &
 done
 #等待所有后台进程执行完成,才会继续执行下面的操作
 wait
+awk '/SectorNumber:/{$1="";SectorNumber=$0} /Activation:/{$1="";Activation=$0} /Expiration:/{$1="";Expiration=$0} /InitialPledge:/{$1="";InitialPledge=$0} /ExpectedDayReward:/{$1="";ExpectedDayReward=$0} /ExpectedStoragePledge:/{$1="";ExpectedStoragePledge=$0} /Partition:/ {print SectorNumber,Activation,Expiration,InitialPledge,ExpectedDayReward,ExpectedStoragePledge;SectorNumber="";Activation="";Expiration="";InitialPledge="";ExpectedDayReward="";ExpectedStoragePledge=""}' $ActiveStateSector |sed 's#^[ ]*##g' > $ActiveSectorInfo
+for i in `awk -F: '{print $1}' $ActiveSectorOut`
+do
+	read -u100
+	{
+		lotus state sector $MinerID $i >> $TerminatedStateSector
+		echo >&100
+	} &
+done
+wait
+awk '/SectorNumber:/{$1="";SectorNumber=$0} /Activation:/{$1="";Activation=$0} /Expiration:/{$1="";Expiration=$0} /InitialPledge:/{$1="";InitialPledge=$0} /ExpectedDayReward:/{$1="";ExpectedDayReward=$0} /ExpectedStoragePledge:/{$1="";ExpectedStoragePledge=$0} /Partition:/ {print SectorNumber,Activation,Expiration,InitialPledge,ExpectedDayReward,ExpectedStoragePledge;SectorNumber="";Activation="";Expiration="";InitialPledge="";ExpectedDayReward="";ExpectedStoragePledge=""}' $TerminatedStateSector |sed 's#^[ ]*##g' > $TerminatedSectorInfo
+sort $TerminatedSectorInfo $ActiveSectorInfo |uniq > $AllSectorInfo
 #排序统计
 n=`wc -l < $AllSectorInfo`
 m=`wc -l < $ActiveSectorInfo`
