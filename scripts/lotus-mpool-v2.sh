25cf5c53f8bcf6069d162987dd90055c898c6949
Gzhichao <zhichaoguo75@gmail.com>
2024-05-20 17:57:03 +0800

    update

--- scripts/lotus-mpool-v2.sh
+++ scripts/lotus-mpool-v2.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
-#堵塞的消息文件
-MPoolMessageInfo="/tmp/MpoolMessageInfo"
+#定义的消息文件
+MPoolMessageInfo="/tmp/MPoolMessageInfo"
+#定义的日志文件
+LogFile="/tmp/LogFile"
 #刷新环境变量
-source /opt/raid0/profile
+source /opt/raid0/profile &>/dev/null
 #检查jq是否安装
 dpkg -s jq  &>/dev/null || apt -y install jq
 #导出消息到文件
-lotus mpool pending --local |jq -r '.Message.Method,.CID' > $MPoolMessageInfo
+lotus mpool pending --local |jq -r '.Message|.To,.From,.Nonce,.Method,.CID."/"' |awk '{if(NR%5==0){print $0}else{printf "%s ",$0}}' > $MPoolMessageInfo
+#消息上链需要一定的时间
+[ -s "$MPoolMessageInfo" ] && sleep 60s || exit 1
+#刷新导出消息的文件
+lotus mpool pending --local |jq -r '.Message|.To,.From,.Nonce,.Method,.CID."/"' |awk '{if(NR%5==0){print $0}else{printf "%s ",$0}}' > $MPoolMessageInfo
 #遍历导出的消息
-while [ -s "$MPoolMessageInfo" ]
+while read Receiver Sender Nonce Method CID
 do
-	#消息上链需要一定的时间
-	sleep 60s
-        #记录当前的时间
-        date +%F%\t%T
-        while [ -s "$MPoolMessageInfo" ]
-	do
-	    #消息类型
-            Method=$(sed -n '1p' $MPoolMessageInfo)
-	    #消息的CID
-            CID=$(awk -F'"' 'NR==3{print $(NF-1)}' $MPoolMessageInfo)
-	    #删除前四行
-            sed -i '1,4d' $MPoolMessageInfo
-	    #如果满足是5类消息，就自动疏通
-            if [ "$Method" -eq 5 ];then
-                  lotus mpool replace --auto --fee-limit 0.3 $CID && echo "$Method类消息的CID: $CID 已疏通"
-		  sleep 1s
-            fi
-        done
-	echo ""
-done
+	#如果满足是5类消息，就自动疏通
+	if [ "$Method" -eq 5 ];then
+		LimitFee="0.006" #第1次疏通限定的数值,如果第一次疏通因为gas的波动超出限定的数值而导致的失败，会进行再次的疏通
+		echo "$Receiver的Nonce:$Nonce第1次疏通: lotus mpool replace --auto --fee-limit $LimitFee $CID" &>> $LogFile
+		lotus mpool replace --auto --fee-limit $LimitFee $CID &>> $LogFile && {
+			echo "$Receiver $Method类消息的CID: $CID 的 Nonce: $Nonce 已疏通(限定的--fee-limit: $LimitFee)" &>> $LogFile
+		}||{
+			if grep -wq "no pending message found from $Sender with nonce $Nonce" $LogFile;then
+                                continue
+                        fi
+			if grep -wq "failed to push new message to mempool: failed to add locked: message from $Sender with nonce $Nonce already in mpool: message with nonce already exists" $LogFile;then
+				continue
+			fi
+			LimitFee="0.2" #第2次疏通限定的数值
+		        echo "$Receiver的Nonce:$Nonce第2次疏通: lotus mpool replace --auto --fee-limit $LimitFee $CID" &>> $LogFile
+		        lotus mpool replace --auto --fee-limit $LimitFee $CID &>> $LogFile && {
+				echo "$Receiver $Method类消息的CID: $CID 的 Nonce: $Nonce 已疏通(限定的--fee-limit: $LimitFee)" &>> $LogFile
+			}
+		}
+	else
+		echo "$Receiver $Method类消息的CID: $CID 的 Nonce: $Nonce 不满足条件，无需操作" &>> $LogFile
+	fi
+done < $MPoolMessageInfo
+echo "" &>> $LogFile
+exit 3
