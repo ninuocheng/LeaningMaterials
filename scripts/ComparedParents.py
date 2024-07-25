120efc5fb56366eb78601cac8f8854f61b708182
ninuocheng <ninuocheng@163.com>
2024-07-25 15:40:16 +0800

    update

--- scripts/ComparedParents.py
+++ scripts/ComparedParents.py
@@ -10,11 +10,13 @@ MinerID = sys.argv[2] # 节点号
 DateTime = sys.argv[3] # 查询日期
 LotteryNum = sys.argv[4] # 抽奖数量
 WinNum = sys.argv[5] # 中奖数量
+RebaseNum = sys.argv[6] # 重做数量
 
 filename = f'{Script}/HashHeightParentsParentBlockHeight'
 with open(filename,mode='r',encoding='utf-8') as rfp:
     content = rfp.readlines()
 HeightParents = {}
+ParentsHeight = {}
 for item in content:
     line = item.split('\t')
     HashHeightParents = line[0].split(' ')
@@ -31,60 +33,124 @@ for item in content:
         try:
             with urllib.request.urlopen(req) as response:
                 content = json.load(response)
+                break
         except:
-            print(f'{url}访问异常，请耐心等待片刻...')
             time.sleep(5)
             continue
-        else:
-            break
     data = content['data']
     if str(data) == 'None':
         HeightParents[MinedHeight] = [MinedParents,ParentBlockHeight]
+        ParentsHeight[MinedParents,ParentBlockHeight] = [MinedHeight]
 
 OrphanNum = len(HeightParents) # 孤块数量
-BlockNum = int(WinNum) - OrphanNum # 出块数量
+BlockNum = int(WinNum) - OrphanNum - int(RebaseNum) # 出块数量
 print(f'{DateTime} {MinerID} 抽奖数量: {LotteryNum} 中奖数量: {WinNum} 出块数量: {BlockNum} 孤块数量: {OrphanNum}')
-if HeightParents:
-    for height,parents in HeightParents.items():
+if ParentsHeight:
+    for parents,height in ParentsHeight.items():
         parentsBlock = parents[0]
-        parentsHeight = parents[1]
-        url = f'https://api.filutils.com/api/v2/tipset/{parentsHeight}'
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-        }
-        req = urllib.request.Request(url=url, headers=headers)
+        parentsHeight = int(parents[1].replace('\n', ''))
+        blockHeight = height[0]
+        height = int(height[0])
         while True:
-            try:
-                with urllib.request.urlopen(req) as response:
-                    content = json.load(response)
-            except:
-                print(f'{url}访问异常，请耐心等待...')
-                time.sleep(10)
+            url = f'https://api.filutils.com/api/v2/tipset/{height-1}'
+            headers = {
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+                    }
+            req = urllib.request.Request(url=url, headers=headers)
+            while True:
+                try:
+                    with urllib.request.urlopen(req) as response:
+                        content = json.load(response)
+                        break
+                except:
+                    time.sleep(10)
+                    continue
+            onChain_parents_lst = []
+            blocks_lst = content['data']['blocks']
+            if str(blocks_lst) == 'None':
+                height = height - 1
                 continue
-            else:
-                break
-        onChain_parents_lst = []
-        blocks_lst = content['data']['blocks']
-        if str(blocks_lst) == 'None':
-            print(f'请求{url}获取到的数据为{content},请检查')
-            continue
+            break
         blocks_lst = content['data']['blocks']
         for item in blocks_lst:
             onChain_parents_lst.append(item['miner'])
-        winning_parents_lst = list(eval(parentsBlock))
+        winning_parents_lst = json.loads(parentsBlock)
+        print(f'WinningParents: {winning_parents_lst}')
         pubParents = set(winning_parents_lst) & set(onChain_parents_lst)  # 交集
         lackParents = set(onChain_parents_lst) - set(winning_parents_lst)  # 差集(缺少)
         addParents = set(winning_parents_lst) - set(onChain_parents_lst)  # 差集(多了)
-        print(f'WinningParents: {winning_parents_lst}')
-        print(f'OnChainParents: {onChain_parents_lst}')
-        if not pubParents:
-            print(f'{MinerID} {height} 父区块同步错误(获取到的父区块高度: {parentsHeight})')
-            continue
-        if set(winning_parents_lst) == set(onChain_parents_lst):
-            print(f'{MinerID} {height} 可能是爬取的页码不够，或是上链超时，或是lotus分叉所致，请检查确认')
-        if lackParents:
-            lackParents = str(lackParents).replace('\'', '').replace('{', '').replace('}', '')
-            print(f'{MinerID} {height} 缺少了父区块{lackParents}')
-        if addParents:
-            addParents = str(addParents).replace('\'', '').replace('{', '').replace('}', '')
-            print(f'{MinerID} {height} 多同步了父区块{addParents}')
+        url = 'https://api.filutils.com/api/v2/orphanblock'
+        headers = {
+            'content-type': 'application/json;charset=UTF-8',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+        }
+        data = {
+            'height': 0,
+            'miner': MinerID,
+            'pageIndex': 1,
+            'pageSize': 20,
+            'type': 0
+        }
+        data = json.dumps(data).encode('utf-8')
+        req = urllib.request.Request(url=url, headers=headers, data=data)
+        while True:
+            try:
+                with urllib.request.urlopen(req) as res:
+                    content = json.load(res)
+                    break
+            except:
+                time.sleep(5)
+                continue
+        data = content['data']
+        height_list = []  # 孤块高度列表
+        for item in data:
+            chainHeight = item['height']
+            height_list.append(chainHeight)
+        if int(blockHeight) not in height_list:
+            print(f'OnChainParents: {onChain_parents_lst}')
+            print(f'{MinerID} {blockHeight} 链上查不到该孤块')
+        else:
+            if height - 1 == parentsHeight:             # 同步到的父区块高度正常
+                print(f'OnChainParents: {onChain_parents_lst}')
+                if lackParents and pubParents:
+                    lackParents = str(lackParents).replace('\'', '').replace('{', '').replace('}', '')
+                    print(f'{MinerID} {blockHeight} 缺少了父区块{lackParents}')
+                if addParents and pubParents:
+                    addParents = str(addParents).replace('\'', '').replace('{', '').replace('}', '')
+                    print(f'{MinerID} {blockHeight} 多同步了父区块{addParents}')
+                if not pubParents:
+                    print(f'{MinerID} {blockHeight} 父区块同步错误')
+                if set(winning_parents_lst) == set(onChain_parents_lst):
+                    print(f'{MinerID} {blockHeight} 上链超时或是lotus分叉或是触发了双叉挖掘故障，请检查确认')
+            else:     # 同步到的父区块高度不正常
+                url = f'https://api.filutils.com/api/v2/tipset/{parentsHeight}'
+                headers = {
+                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+                }
+                req = urllib.request.Request(url=url, headers=headers)
+                while True:
+                    try:
+                        with urllib.request.urlopen(req) as response:
+                            content = json.load(response)
+                            break
+                    except:
+                        time.sleep(10)
+                        continue
+                onChain_parents_lst = []
+                blocks_lst = content['data']['blocks']
+                for item in blocks_lst:
+                    onChain_parents_lst.append(item['miner'])
+                print(f'OnChainParents: {onChain_parents_lst}')
+                pubParents = set(winning_parents_lst) & set(onChain_parents_lst)  # 交集
+                lackParents = set(onChain_parents_lst) - set(winning_parents_lst)  # 差集(缺少)
+                addParents = set(winning_parents_lst) - set(onChain_parents_lst)  # 差集(多了)
+                if lackParents and pubParents:
+                    lackParents = str(lackParents).replace('\'', '').replace('{', '').replace('}', '')
+                    print(f'{MinerID} {blockHeight} 缺少了父区块{lackParents}(同步到的父区块高度是{parentsHeight})')
+                if addParents and pubParents:
+                    addParents = str(addParents).replace('\'', '').replace('{', '').replace('}', '')
+                    print(f'{MinerID} {blockHeight} 多同步了父区块{addParents}(同步到的父区块高度是{parentsHeight})')
+                if not pubParents:
+                    print(f'{MinerID} {blockHeight} 父区块同步错误(同步到的父区块高度是{parentsHeight})')
+                if set(winning_parents_lst) == set(onChain_parents_lst):
+                    print(f'{MinerID} {blockHeight} 同步到的父区块高度是{parentsHeight}')
--- scripts/OrphanBlock.sh
+++ scripts/OrphanBlock.sh
@@ -17,19 +17,30 @@ do
     HashHeightParents="$ScriptDir/HashHeightParents"
     WinParentBlockHeight="$ScriptDir/WinParentBlockHeight"
     HashHeightParentsParentBlockHeight="$ScriptDir/HashHeightParentsParentBlockHeight"
+    IsWinner="$ScriptDir/IsWinner"
+    IsWinERROR="$ScriptDir/IsWinERROR"
     [ ! -f "/opt/raid0/lotusminer-winning/lotusminer/logs" ] && echo "日志文件不存在，请检查" && exit
     grep -ar 'mined new block.*{"cid":' $WinningLog |grep "${DateTime}T" > $MinedNewBlock
-    LotteryNum=$(grep -ar '"isEligible": true, "isWinner": false' $WinningLog |grep "${DateTime}T" -c) #抽奖数量
+    grep -ar '"isEligible": true, "isWinner": true' $WinningLog |grep "${DateTime}T" > $IsWinner
+    LotteryNum=$(grep -ar '"isEligible": true, "isWinner":' $WinningLog |grep "${DateTime}T" -c) #抽奖数量
     WinNum=$(grep -ar '"isEligible": true, "isWinner": true'  $WinningLog |grep "${DateTime}T" -c) #中奖数量
     # 过滤区块哈希，区块高度，父区块到一个文件
     awk '{print $9,$11,$15}' $MinedNewBlock |sed 's#,$##' > $HashHeightParents
     > $WinParentBlockHeight
-    for forRound in $(awk '{print $2}' $HashHeightParents |sed 's#,##')
+    # 中奖数和出块数匹配要一致，否则需要处理掉,一般都是区块高度不同步，再次重做所致
+    IsWinNum=$(wc -l < $IsWinner)
+    MinedNum=$(wc -l < $MinedNewBlock)
+    n=$[IsWinNum-MinedNum]
+    if [ "$n" -ne 0 ];then
+	    sed -i '/ERROR/d' $IsWinner
+    fi
+    for forRound in $(awk '{print $2}' $HashHeightParents |sed 's#,##' |sort |uniq)
     do
 	    # 过滤区块高度的父区块高度到一个文件
-            grep -ar '"isEligible": true, "isWinner": true' $WinningLog |grep "${DateTime}T" |awk '/"forRound": '$forRound'/{print $12}' |sed 's#,##' >> $WinParentBlockHeight
+            awk '/"forRound": '$forRound'/{print $12}' $IsWinner|sed 's#,##' >> $WinParentBlockHeight
     done
     # 合并区块哈希，区块高度，父区块， 父区块高度到一个文件
     paste $HashHeightParents $WinParentBlockHeight > $HashHeightParentsParentBlockHeight
-    python3 $ScriptDir/ComparedParents.py $ScriptDir $MinerID $DateTime $LotteryNum $WinNum
+    m=$(awk '{print $2}' $HashHeightParentsParentBlockHeight |uniq -c |awk '{sum+=$1-1}END{print sum}')
+    python3 $ScriptDir/ComparedParents.py $ScriptDir $MinerID $DateTime $LotteryNum $WinNum $[n+m]
 done
